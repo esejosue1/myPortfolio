bef8440f8a32b8bfc5c8f4e7ef1257d1ac9c253b
josue lara <josuegallardolara@hotmail.com>
2023-02-17 09:55:25 -0800

    Deployment

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=True, cast=bool)
 
 
-#ALLOWED_HOSTS = ['jg-portfolio-env.eba-riasytrm.us-west-2.elasticbeanstalk.com']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['JGportfolio-env.eba-ag3pdvsj.us-west-2.elasticbeanstalk.com']
+#ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
